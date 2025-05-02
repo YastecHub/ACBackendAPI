3cad26a0cde105b4fb841e198652eec93de6e74e
Yasir Oyebo <yasiroyebo@gmail.com>
2025-05-02 18:36:06 +0100

    feat:complete implementation for authentication

---
+++ ACBackendAPI.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using ACBackendAPI.Application.Dtos;
+using ACBackendAPI.Application.Interfaces.IServices;
+using ACBackendAPI.Persistence.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACBackendAPI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register-admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] AdminDto adminDto)
+        {
+            var response = await _authService.RegisterAdmin(adminDto);
+            if (!response.Success)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("register-student")]
+        public async Task<IActionResult> RegisterStudent([FromBody] StudentDto studentDto)
+        {
+            var response = await _authService.RegisterStudent(studentDto);
+            if (!response.Success)
+                return BadRequest(response);
+
+            return Ok(response);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var response = await _authService.Login(loginDto);
+            if (!response.Success)
+                return Unauthorized(response);
+
+            return Ok(response);
+        }
+    }
+}
--- ACBackendAPI.WebApi/Program.cs
+++ ACBackendAPI.WebApi/Program.cs
@@ -1,13 +1,41 @@
+using ACBackendAPI.Application.Interfaces.IRepositories;
+using ACBackendAPI.Application.Interfaces.IServices;
+using ACBackendAPI.Domain.Entities;
 using ACBackendAPI.Persistence.Context;
+using ACBackendAPI.Persistence.Repositories.Repository;
+using ACBackendAPI.Persistence.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                      .AllowAnyMethod()
+                      .AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped(typeof(IAsyncRepository<,>), typeof(EfRepository<,>));
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IProgrammeService, ProgrammeService>();
 
 var app = builder.Build();
 
@@ -15,12 +43,18 @@ using var scope = app.Services.CreateScope();
 var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
 await RoleSeeder.SeedRolesAsync(roleManager);
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "ACBackendAPI v1");
+        options.DocumentTitle = "AC Backend API Docs";
+    });
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
