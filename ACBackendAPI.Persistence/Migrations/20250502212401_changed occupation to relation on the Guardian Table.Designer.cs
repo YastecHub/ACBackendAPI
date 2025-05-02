4aa8e92da31f2a32606b050e5095e80188a6312d
Yasir Oyebo <yasiroyebo@gmail.com>
2025-05-02 22:35:20 +0100

    fix:edit and added new migration for the guardian table

--- ACBackendAPI.Persistence/Migrations/20250502212401_changed occupation to relation on the Guardian Table.Designer.cs
+++ ACBackendAPI.Persistence/Migrations/20250502212401_changed occupation to relation on the Guardian Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ACBackendAPI.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250502212401_changed occupation to relation on the Guardian Table")]
+    partial class changedoccupationtorelationontheGuardianTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
