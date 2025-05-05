29d2eb45fcc8b824bcb355ad92a542cc0fa7612f
Yasir Oyebo <yasiroyebo@gmail.com>
2025-05-05 20:34:54 +0100

    fix:fixed the date issues

--- ACBackendAPI.Domain/Entities/Programme.cs
+++ ACBackendAPI.Domain/Entities/Programme.cs
@@ -3,7 +3,7 @@
     public class Programme : BaseEntity
     {
         public string ProgrammeName { get; set; }
-        public long ProgrammeFee { get; set; }
+        public decimal ProgrammeFee { get; set; }
         public string ProgrammeDescription { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
